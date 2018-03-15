191d9e8ef44e9ca1ddc27503bd979650d544d6f6
klawuy <kevin.lawuy@gmail.com>
2018-03-15 09:17:00 +0100

    Ajout des jcfields

--- mod_etd_article.php
+++ mod_etd_article.php
@@ -12,13 +12,15 @@ defined('_JEXEC') or die;
 // Include the archive functions only once
 JLoader::register('ModEtdArticleHelper', __DIR__ . '/helper.php');
 
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
+$moduleclass_sfx   = htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
 $result            = ModEtdArticleHelper::getArticle($params);
+$dispatcher        = JEventDispatcher::getInstance();
 
 if ($result) {
 
     if ($params->def('prepare_content', 1)) {
         JPluginHelper::importPlugin('content');
+        $dispatcher->trigger('onContentPrepare', array ('com_content.article', &$result, &$params));
         $result->introtext = JHtml::_('content.prepare', $result->introtext, '', 'mod_etd_article.introtext');
     }
 
